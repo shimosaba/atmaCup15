d95b03edc27f0bab8aa20c2d778f0201c7a3d2ae
shimosaba <78221957+shimosaba@users.noreply.github.com>
2023-07-24 08:28:34 +0900

    Update exp.sh

--- main/exp.sh
+++ main/exp.sh
@@ -1,7 +1,5 @@
-# python3 seen_exp.py 42
-# python3 seen_exp.py 21
-# python3 seen_exp.py 2023
-# python3 unseen_exp.py 42
+python3 seen_exp.py 42
+python3 seen_exp.py 21
+python3 unseen_exp.py 42
 python3 unseen_exp.py 21
-# python3 unseen_exp.py 2023
 python3 make_submission.py
